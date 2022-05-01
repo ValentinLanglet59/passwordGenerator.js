0b81e9641bbe5140cb330b7e8d533dadd8afe53a
ValentinLanglet59 <valdu59960@hotmail.fr>
2022-05-01 17:13:05 +0200

    done

--- index.js
+++ index.js
@@ -12,25 +12,6 @@ let buttonClicked2 = true
 let buttonClicked3 = true
 let buttonClicked4 = true
 
-
-
-// firstPasswordEl.textContent = generatePasswords()
- //secondPasswordEl.textContent = generatePasswords();
-// thirdPasswordEl.textContent = generatePasswords()
-// fourthPasswordEl.textContent = generatePasswords()
-
-
-//function generatePasswords() {
-    //for(let i = 0; i <= 8; i++) {
-        //let randomNumbers = randomChars[Math.floor(Math.random()* 25)]
-        //firstPasswordEl.textContent += randomNumbers
-        //secondPasswordEl.textContent += randomNumbers
-        //thirdPasswordEl.textContent += randomNumbers
-        //fourthPasswordEl.textContent += randomNumbers
-        //randomResult.Math.floor(Math.random()* 10 )
-    //}
-//}
-
 function generatePasswords() {
     if (buttonClicked === true) {
         for(let i = 0; i <= 8; i++) {
@@ -38,30 +19,12 @@ function generatePasswords() {
             result.push(randomResult)
             resultEl.textContent += randomResult
             buttonClicked = false
+        }
     }
-        //firstPasswordEl.textContent += randomNumbers
-        //secondPasswordEl.textContent += randomNumbers
-        //thirdPasswordEl.textContent += randomNumbers
-        //fourthPasswordEl.textContent += randomNumbers
-        //randomResult.Math.floor(Math.random()* 10 )
-    }
-    //return result
     generatePasswords2()
     generatePasswords3()
     generatePasswords4()
 }
-// console.log(generatePasswords())
-// var item = items[Math.floor(Math.random()*items.length)];
-
-
-//function generatePasswords() {
-    //for(let i = 0; i < randomChars.length; i++) {
-        //let randomNumbers = randomChars[Math.floor(Math.random()* 25)]
-        //firstPasswordEl.textContent += randomNumbers
-        //randomResult.Math.floor(Math.random()* 10 )
-    //}
-//}
-
 
 function generatePasswords2() {
     if (buttonClicked2 === true) {
@@ -71,12 +34,7 @@ function generatePasswords2() {
         console.log(result)
         secondPasswordEl.textContent += randomResult
         buttonClicked2 = false
-    }
-        //firstPasswordEl.textContent += randomNumbers
-        //secondPasswordEl.textContent += randomNumbers
-        //thirdPasswordEl.textContent += randomNumbers
-        //fourthPasswordEl.textContent += randomNumbers
-        //randomResult.Math.floor(Math.random()* 10 )
+        }   
     }
     return result
 }
@@ -89,12 +47,7 @@ function generatePasswords3() {
             console.log(result)
             thirdPasswordEl.textContent += randomResult
             buttonClicked3 = false
-    }
-        //firstPasswordEl.textContent += randomNumbers
-        //secondPasswordEl.textContent += randomNumbers
-        //thirdPasswordEl.textContent += randomNumbers
-        //fourthPasswordEl.textContent += randomNumbers
-        //randomResult.Math.floor(Math.random()* 10 )
+        }
     }
     return result
 }
@@ -107,12 +60,7 @@ function generatePasswords4() {
             console.log(result)
             fourthPasswordEl.textContent += randomResult
             buttonClicked4 = false
-    }
-        //firstPasswordEl.textContent += randomNumbers
-        //secondPasswordEl.textContent += randomNumbers
-        //thirdPasswordEl.textContent += randomNumbers
-        //fourthPasswordEl.textContent += randomNumbers
-        //randomResult.Math.floor(Math.random()* 10 )
+        }
     }
     return result
 }
